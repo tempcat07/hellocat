59658482ac4c16b023d5da205e161a0cba32ad08
tempcat07 <tempcat07@gmail.com>
2020-12-08 20:18:17 +0300

    годное решение с тестовым принтом работы

--- test.py
+++ test.py
@@ -1,36 +1,43 @@
-def get_median(test):
-    test.sort()
-    lentest = len(test)
-    middle = int(lentest/2)
-    if  lentest % 2 == 0:
-        return int(sum(test[middle-1:middle+1])/2)
-    else:  return test[middle]
+import numpy as np
+count = 0                            # счетчик попыток
+number = np.random.randint(1,101)    # загадали число
+print ("Загадано число от 1 до 100")
 
+def score_game(game_core):
+    '''Запускаем игру 1000 раз, чтобы узнать, как быстро игра угадывает число'''
+    count_ls = []
+    np.random.seed(1)  # фиксируем RANDOM SEED, чтобы ваш эксперимент был воспроизводим!
+    random_array = np.random.randint(1,101, size=(1000))
+    for number in random_array:
+        count_ls.append(game_core(number))
+    score = int(np.mean(count_ls))
+    print(f"Ваш алгоритм угадывает число в среднем за {score} попыток")
+    return(score)
+# запускаем
 
-
-    #программу, которая создаёт список my_list, содержащий длины слов из списка raw_list (дан в начале кода),
-#и находит сумму минимального и максимального элементов нового списка (my_list).
-#Результат вычислений (сумма минимального и максимального элементов списка my_list)
-#присвойте переменной result.
-raw_list = ['переменные', 'циклы', 'условия', 'списки', 'словари', 'файлы', 'функции']
-my_list=[]
-for i in raw_list:
-    my_list.append(len(i))
-my_list.sort()
-result=my_list[0]+my_list[-1]
-my_list
-result
-
-def count_letters(sentence, average=False):
-    listn=[]
-    long= sentence.split()
-    result=0
-    if average==False:
-        for n in long:
-        	listn.append(len(n))
+def game_core_tempcat(number):
+    
+    '''Сначала устанавливаем число равное середине  возможного диапазона, а потом уменьшаем или увеличиваем его в зависимости 
+    от того, больше оно или меньше нужного, но уже опираясь на правило, что число будет находится в середине 
+    предполагаемого диапазона исходя из предыдущих шагов.
+            Функция принимает загаданное число и возвращает число попыток'''
+    
+    count = 1      # счетчик попыток
+    predict = 50   # указатель стремящийся к загаданному числу (и станет им)
+    limax = 101    # верхний предел + 1  
+    limin = 0      # нижний предел -1
+    
+    while number != predict:
+        print(number,predict)
+        count+=1
+        if number > predict:
+            limin = predict
+            predict += (limax - limin)/2
             
-        result=listn
-    return result
-
-#do not touch m
+        elif number < predict:
+            limax = predict
+            predict -= (limax - limin)/2
+    return(count)   # выход из цикла, если угадали
+                    # Проверяем
+score_game(game_core_tempcat)
     
